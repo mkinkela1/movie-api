27fa1def72f42d0621856e83764fabebadf11f24
Matteo Kinkela <matteo.kinkela@q-software.com>
2020-10-02 03:45:44 +0200

    fix(css): design flaws

--- src/pages/Home/components/MovieContainer/MovieContainer.tsx
+++ src/pages/Home/components/MovieContainer/MovieContainer.tsx
@@ -12,29 +12,27 @@ interface Movie {
 
 const MovieContainer: FunctionComponent<Movie> = props => {
   return (
-    <>
-      <div className="l-grid__row l-grid__row-3">
-        <Link
-          className="c-movie-container"
-          to={location => ({ ...location, pathname: `/movie/${props.id}` })}>
-          <div className="c-movie-container__rate">
-            {props.vote_average.toFixed(1)}
+    <div className="s-movies__item">
+      <Link
+        className="c-movie-container"
+        to={location => ({ ...location, pathname: `/movie/${props.id}` })}>
+        <div className="c-movie-container__rate">
+          {props.vote_average.toFixed(1)}
+        </div>
+        <img
+          src={`${process.env.IMAGE_URL_W500}/${props.poster_path}`}
+          className="c-movie-container__poster"
+          alt="Movie poster"
+        />
+        <div className="data">
+          {`${props.title} (${props.release_date?.substr(0, 4)})`}
+          <div className="data__language">
+            Language:&nbsp;
+            {props.original_language}
           </div>
-          <img
-            src={`${process.env.IMAGE_URL_W500}/${props.poster_path}`}
-            className="c-movie-container__poster"
-            alt="Movie poster"
-          />
-          <div className="data">
-            {`${props.title} (${props.release_date?.substr(0, 4)})`}
-            <div className="data__language">
-              Language:&nbsp;
-              {props.original_language}
-            </div>
-          </div>
-        </Link>
-      </div>
-    </>
+        </div>
+      </Link>
+    </div>
   );
 };
 
